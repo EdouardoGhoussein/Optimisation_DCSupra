bb2dcc6c6b1a0b51fbde32d5b0d52dc7ea751c7e
aminem7777777 <mettaliamine@gmail.com>
2025-03-05 11:19:53 +0100

    add fonction objectif

---
+++ ObjContr.m
@@ -0,0 +1,15 @@
+function [Objectifs, Contraintes,Divers]= ObjContr(Essaim)
+l,c=size(Essaim);
+Objectifs=zeros(l,1);
+Contraintes=zeros(l,1);
+Divers=[];
+
+
+for index  = 1:l 
+    result(i,1)=-Essaim(i,1)
+    
+    
+end
+
+end
+    
